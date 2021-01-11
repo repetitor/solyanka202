ea8ed2cb676279379e617db879e35529a324db5f
Viktar <repetitor202@gmail.com>
2021-01-11 23:05:45 +0300

    php-clickhouse

--- sandbox/app-php/src/App.php
+++ sandbox/app-php/src/App.php
@@ -2,12 +2,18 @@
 
 namespace App;
 
+use App\Services\EnvService;
+use Repetitor202\ClickhouseDraft;
 use Repetitor202\Test;
 
 class App
 {
     public function run()
     {
+        EnvService::init();
+
+        //tests
         new Test();
+        (new ClickhouseDraft())->test();
     }
 }
