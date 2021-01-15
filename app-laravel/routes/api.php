6fa2abcc02b24af857584d20cebd294a5895370a
Viktar <repetitor202@gmail.com>
2021-01-15 22:54:38 +0300

    app-laravel get-public-test

--- app-laravel/routes/api.php
+++ app-laravel/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('get-public-test', function (Request $request) {
+//    return $request;
+//});
+Route::get('get-public-test', 'App\Http\Controllers\TestController@getPublicTest');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
